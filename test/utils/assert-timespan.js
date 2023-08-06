425b8efc8e32b534855b75c173b3e33152a7998d
uzlopak <aras.abbasi@googlemail.com>
2023-08-06 20:37:16 +0200

    increase tolerance to 15%

--- test/utils/assert-timespan.js
+++ test/utils/assert-timespan.js
@@ -5,9 +5,9 @@
  * @param {number} start
  * @param {number} end
  * @param {number} expected
- * @param {number} [tolerance=10]
+ * @param {number} [tolerance=15]
  */
-function assertTimespan (t, start, end, expected, tolerance = 10) {
+function assertTimespan (t, start, end, expected, tolerance = 15) {
   const diff = end - start
   const delta = Math.abs(expected - diff)
   t.ok(delta <= (expected / 100 * tolerance), 'tolerance of ' + tolerance + '%, got ' + delta + 'ms')
