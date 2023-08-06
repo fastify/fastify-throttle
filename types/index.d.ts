3dc444f5c0329daac558bb370047eaf13c204dd9
uzlopak <aras.abbasi@googlemail.com>
2023-08-06 17:39:46 +0200

    improve typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,24 +12,52 @@ declare module 'fastify' {
 
 type FastifyThrottle = FastifyPluginCallback<fastifyThrottle.FastifyThrottleOptions>;
 
+/**
+ * Represents a function that calculates the rate of bytes per second.
+ * @callback BytesPerSecondFn
+ * @param {number} elapsedTime - The elapsed time in seconds.
+ * @param {number} bytes - The number of bytes processed during the elapsed time.
+ * @returns {number} The rate of bytes per second as a number.
+ */
+type BytesPerSecondFn = (elapsedTime: number, bytes: number) => number
+
+/**
+ * Namespace for fastify-throttle plugin options.
+ *
+ * @namespace fastifyThrottle
+ */
 declare namespace fastifyThrottle {
 
+  /**
+   * Options for configuring the fastify-throttle plugin.
+   *
+   * @interface FastifyThrottleOptions
+   */
   export interface FastifyThrottleOptions {
-    bytesPerSecond: number | ((elapsedTime: number, bytes: number) => number)
     /**
-     * Throttle stream payloads
+     * Number or a function that returns bytes per second to throttle to.
+     * @type {number|BytesPerSecondFn}
+     * @default 16384
+     */
+    bytesPerSecond: number | BytesPerSecondFn
+
+    /**
+     * Throttle stream payloads.
+     * @type {boolean}
      * @default true
      */
     streamPayloads?: boolean
 
     /**
-     * Throttle buffer payloads
+     * Throttle buffer payloads.
+     * @type {boolean}
      * @default false
      */
     bufferPayloads?: boolean
 
     /**
-     * Throttle string payloads
+     * Throttle string payloads.
+     * @type {boolean}
      * @default false
      */
     stringPayloads?: boolean
